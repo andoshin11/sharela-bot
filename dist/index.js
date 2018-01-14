e1466ebd370e290624eea26fe2260251034d679a
andoshin11 <shinglish11@gmail.com>
2018-01-15 08:08:33 +0900

    Ignore dist folder

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-require('dotenv').config();
-var handler = exports.handler = function handler(req, res) {
-  return res.send(process.env.SECRET_MSG);
-};
